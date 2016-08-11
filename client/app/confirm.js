651419317a31e514faf21a810d2108d9e1ac1f5f
ToyeOnikoyi <mycomputer@mycomputersmbp.devices.brown.edu>
2016-08-10 20:03:34 -0400

    added count down package

--- client/app/confirm.js
+++ client/app/confirm.js
@@ -1,11 +1,30 @@
 Template.confirm.onCreated( () => {
 
 Meteor.subscribe('myStreaks');
+var countdown = new ReactiveCountdown(24, {
+  // Value substracted every tick from the current countdown value
+ steps: 1,
+
+ // Specify the countdown's interval in milliseconds
+ interval: 3600000,
+
+ // Callback: Tick, called on every interval
+ tick: function() {},
+
+ // Callback: Complete, called when the countdown has reached 0
+ completed: function() {},
+
+
+})
 
 });
 
 Template.confirm.helpers({
 
+getCountdown () {
+
+  return countdown.get();
+}
 
 });
 
@@ -13,11 +32,17 @@ Template.confirm.events({
 'click .confirm': function(event){
   event.preventDefault();
 var noStreaks = Streaks.find().count();
+countdown.start(function(){
 
+
+});
   Meteor.call('addStreaks');
+
+
   console.log('streaks increassed');
   Router.go('/success');
 },
+
 'click .confirmAgain': function(event){
   event.preventDefault();
 var noStreaks = Streaks.find().count();
