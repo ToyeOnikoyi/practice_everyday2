7ad2280d04a237110dcaebf039374bbf685baa98
bferrell514 <bferrell514@gmail.com>
2016-08-14 19:07:56 -0400

    noEnglish

--- client/app/confirm.js
+++ client/app/confirm.js
@@ -6,7 +6,7 @@ var countdown = new ReactiveCountdown(24, {
  steps: 1,
 
  // Specify the countdown's interval in milliseconds
- interval: 3600000,
+ interval: 3600,
 
  // Callback: Tick, called on every interval
  tick: function() {},
@@ -16,8 +16,14 @@ var countdown = new ReactiveCountdown(24, {
 
 
 })
+countdown.start(function() {
+
+    // do something when this is completed
 
 });
+
+});
+
 
 Template.confirm.helpers({
 
@@ -32,12 +38,22 @@ Template.confirm.events({
 'click .confirm': function(event){
   event.preventDefault();
 var noStreaks = Streaks.find().count();
-countdown.start(function(){
+if(noStreaks == 0){
+    Meteor.call('addStreaks',true);
 
+}
+/*countdown.start(function(){
 
-});
-  Meteor.call('addStreaks');
+//Meteor.call('updateToZero');
+console.log('hello');
+});*/
+//if(countdown.start()){
+      Bert.alert('You cant do that twice in one day','success','growl-top-right');
+//}
 
+if(Streaks.checkStreakToday = false){
+  Meteor.call('addStreaks',true);
+}
 
   console.log('streaks increassed');
   Router.go('/success');
--- server/practiceToday.js
+++ server/practiceToday.js
@@ -8,16 +8,19 @@ return Streaks.find({});
 });
 
 Meteor.methods({
-'addStreaks':function (addNumber){
+'addStreaks':function (flag){
 var currentUser = Meteor.user();
 
 Streaks.upsert({
   owner:currentUser,
-
+  checkStreakToday: flag,
 },{$inc: {streaks:1}});
 
 },
+'updateToZero':function(){
 
+  Streaks.update({},{$set: {streaks:0, checkStreakToday:false}});
+}
 
 
 })
